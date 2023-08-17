87518534dfa1efe2ecb6c3572729233d92924d51
Sandeepa <sandeepatharaka@gmail.com>
2023-08-17 20:19:22 +0530

    frontend code change

--- frontend/src/app/app-service.service.ts
+++ frontend/src/app/app-service.service.ts
@@ -10,12 +10,8 @@ export class AppServiceService {
   readonly ROOT_URL;
 
   constructor(private http: HttpClient) {
-    if(environment.production == false){
-      this.ROOT_URL = 'test'
-    }else{
-      this.ROOT_URL = 'api'
-    }
-  }
+    this.ROOT_URL = 'http://localhost:8080'
+}
 
   initializeDB(){
     return this.http.get(`/${this.ROOT_URL}/dbinitialize`)
